28710be982542d39dc71bec723bd1a3ced11bfc5
Sumit <sumitgupta.1018@gmail.com>
2023-10-06 11:35:41 +0530

    Week1

---
+++ Lecture6 III/Lecture6 III loops.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,n;
+	
+/*	while(i<101)
+	{
+		cout<<i<<" ";
+		i++;
+	}*/
+	cin>>n;
+	i=100;
+	for(i=1;i<101;i++)
+	{
+		if(n==65){
+			cout<<"you have guessed the correct number";
+			break;
+		}
+	}
+}
